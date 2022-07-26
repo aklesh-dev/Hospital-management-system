f5696eb84d9013c324aacb6dc412fd2df48a5554
arzuna-dev <71864013+arzuna-dev@users.noreply.github.com>
2022-07-26 18:37:26 +0545

    added time running on real time
    change footer sytle

--- App/static/js/script.js
+++ App/static/js/script.js
@@ -8,3 +8,11 @@ $(document).ready(function(){
         $("#no-data").text('No patient found !!!');
     }
 });
+
+//--2) Time running at real time
+setInterval(function(){
+    const date = new Date();
+    $("#clock").html(
+        (date.getHours() < 10 ? '0' : '') + date.getHours() + ':' + (date.getMinutes() < 10 ? '0' : '') + date.getMinutes() + ':' + (date.getSeconds() < 10 ? '0' : '') + date.getSeconds()
+    );
+}, 500);
